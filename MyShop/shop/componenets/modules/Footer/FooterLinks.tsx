efdae954a470571d42c08ae665f3c75f220c55f7
Aliaksandr Bykouski <developweb34@gmail.com>
2025-08-26 19:47:23 +0200

    add footer

---
+++ MyShop/shop/componenets/modules/Footer/FooterLinks.tsx
@@ -0,0 +1,14 @@
+const FooterLinks = () => {
+  return (
+    <div className='footer__links'>
+      <span>
+        <a href='https://vc.ru'>vc.ru</a>
+      </span>
+      <span>
+        <a href='https://habr.com'>habr.com</a>
+      </span>
+    </div>
+  )
+}
+
+export default FooterLinks
